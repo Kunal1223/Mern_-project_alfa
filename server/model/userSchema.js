b14b6c1a394e7b7b620803da0b29f13bef18231b
Kunal Kishor <kunalbth2002@gmail.com>
2023-06-17 15:05:16 +0530

    password encrption done

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -21,7 +22,16 @@ const userSchema = new mongoose.Schema({
         type:String,
         require:true
     }
-})
+});
+
+userSchema.pre('save', async function(next){
+    console.log("i am bycrept");
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password , 12);
+        this.cpassword = await bcrypt.hash(this.cpassword , 12);
+    }
+    next();
+});
 
 const User = mongoose.model("USER",userSchema);
 module.exports = User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -9,7 +9,35 @@ router.get("/", (req, res) => {
     res.send("This is a home page for you kunal kishor1 working on router.");
 })
 
-router.post('/resister', (req, res) => {
+//using promises
+// router.post('/resister', (req, res) => {
+
+//     const { name, email, phone, password, cpassword } = req.body;
+
+//     if (!name || !email || !phone || !password || !cpassword) {
+//         return res.status(422).json({ error: "Please fill all the secton" });
+//     }
+
+//     User.findOne({ email: email })
+//         .then((userExist) => {
+//             if (userExist) {
+//                 return res.status(422).json({ error: "You are already resister" });
+//             }
+
+//             const user = new User({ name, email, phone, password, cpassword });//we also write this like {name:name} but according to new convention we directly write {name} instant of {name:name}
+
+//             user.save().then(() => {
+//                 res.status(201).json({ message: "You are Resister succesfully" });
+//             }).catch((err) => res.status(500).json({ error: "Failed to here resister" }));
+
+//         }).catch((error) => { console.log(error) });
+
+//     res.json({ message: req.body });
+// })
+
+
+//by unsing async and await
+router.post('/resister', async (req, res) => {
 
     const { name, email, phone, password, cpassword } = req.body;
 
@@ -17,21 +45,53 @@ router.post('/resister', (req, res) => {
         return res.status(422).json({ error: "Please fill all the secton" });
     }
 
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(401).json({ error: "You are already resister" });
-            }
+    try {
+        const userExist = await User.findOne({ email: email });
+        if (userExist) {
+            return res.status(422).json({ error: "You are already resister" });
+        }
+        const user = new User({ name, email, phone, password, cpassword });//we also write this like {name:name} but according to new convention we directly write {name} instant of {name:name}
 
-            const user = new User({ name, email, phone, password, cpassword });//we also write this like {name:name} but according to new convention we directly write {name} instant of {name:name}
+        const userResister = await user.save();
 
-            user.save().then(() => {
-                res.status(201).json({ message: "You are Resister succesfully" });
-            }).catch((err) => res.status(500).json({ error: "Failed to resister" }));
+        if (userResister) {
+            res.status(201).json({ message: "You are Resister succesfully" });
+        }
 
-        }).catch((error) => { console.log(error) });
+        //upgrade version of above code
+        //  await user.save();
+        //  res.status(201).json({ message: "You are Resister succesfully" });
 
+    } catch (err) {
+        console.log(err)
+    }
     res.json({ message: req.body });
 })
+
+
+////for login part
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(404).json({ error: "Enter incorrect email" });
+        }
+
+        const userLogin = await User.findOne({email:email});
+
+        if(!userLogin){
+            res.status(404).json({error : "user error"});
+        }
+        else{
+            res.status(200).json({error : "user login successfully"});
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+
 
 module.exports = router;
