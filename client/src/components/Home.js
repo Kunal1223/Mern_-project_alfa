4ad50703163fea0a74f22daecaf6f380bfe047eb
Kunal Kishor <kunalbth2002@gmail.com>
2023-06-19 01:02:43 +0530

    little update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1>Home is here for there</h1>
+       <div className="home-page">
+                <div className="home-div">
+                    <p className="pt-5">WELCOME</p>
+                    <h1>I am Kunal Kishor : Welcome you in my website</h1>
+                    {/* <h2> { show ? 'Happy, to see you back' :  'We Are The MERN Developer' }</h2> */}
+                </div>
+            </div>
     </div>
   )
 }
