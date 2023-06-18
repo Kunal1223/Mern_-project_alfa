70fa6f324a7cd4331ad0e3e0624f5959ddc074d5
Kunal Kishor <kunalbth2002@gmail.com>
2023-06-18 22:23:46 +0530

    client page modify

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,6 +7,10 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">Navbar</a>
+
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto me-3" >
                             <li className="nav-item">
